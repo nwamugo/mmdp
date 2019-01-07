0f45ec295a57bf41331bda6e06cd6d3b80ecf6b9
chariss <charisschomba@gmail.com>
2019-01-07 19:14:30 +0300

    feat(about-page): add sub-menu content
    - add About MMDP section
    - add Coordination section
    - add Objectives section
    - add Edo state approach section
    
    [WIP MMDP-41]

---
+++ assets/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $( "#header-section" ).load( "/partials/header.html", function () {
+    //once header is loaded activate sidenav
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    enableMenuItemContentLoad();
+  } );
+  $( "#footer-section" ).load( "/partials/footer.html" );
+});
