ecf3aca81a06759e8b6090d1e2b1ce51c44dec6f
James Chege <jymeschege@gmail.com>
2019-02-26 10:30:34 +0300

    feat(list-docs): integrate documents data from cms
    - fetch docs from cms and display on website
    - download the documents from the website
    - search for documents
    - see paginated results
    [ Delivers MMDP-354]

---
+++ assets/js/cms/resources/documents.js
@@ -0,0 +1,91 @@
+cmsLoad = function () {
+  registerEventHandlers();
+  fetchDocs();
+};
+
+function registerEventHandlers() {
+  const searchForm = $('#docs_search_form');
+  const searchInput = $('#docs_input');
+
+  searchInput.keyup(function () {
+    const search = searchInput.val();
+    if (!search) {
+      fetchDocs(1, search);
+    }
+  });
+
+  searchForm.submit(function (event) {
+    event.preventDefault();
+    const search = searchInput.val();
+    if (search) {
+      fetchDocs(1, search);
+    }
+  })
+}
+
+function fetchDocs(page = 1, search = '') {
+  const params = formatObjectToParams({page: page, title: search});
+  client(`resources/repository/document?${params}`)
+    .then((res) => {
+      res.json().then((res) => {
+        render(res.data);
+        scrollToTop();
+      }).catch((err) => console.log(err));
+    })
+}
+
+function render(data) {
+  renderDocuments(data.documents);
+  renderPagination(data.pagination);
+  scrollToTop();
+}
+
+function renderDocuments(documents) {
+  let docs = '';
+  if (documents && documents.length) {
+    documents && documents.forEach((doc) => {
+      docs += `<div class="research-content__card">
+           <div class="research-content__details">
+             <p class="research-content__essentials">${doc.title}</p>
+             <p class="research-content__date-published">Published: ${formatDate(new Date(doc.created_at))}</p>
+           </div>
+           <div class="research-content__downloading">
+             <a href='${doc.document.url}' target="_blank" download><span class="research-content__download">Download</span></a>
+           </div>
+           <div class="row research-content__devider">
+             <hr class="content-devider">
+           </div>
+         </div>`;
+    });
+  } else {
+    docs =
+      `<div class="card-panel red lighten-5 pagination__no-results">
+          There are no results to display.
+        </div>`
+  }
+  $('#docs_docs').html(docs);
+}
+
+function renderPagination(pagination) {
+  const {total, totalPages, currentPage, previous, next} = pagination;
+
+  let markup = !total || totalPages === 1 ? '' :
+    `<div class="research-content__pagination">
+      <div class="pagination__page">Page</div>
+      <div class="current-page square-shape">${currentPage}</div>
+      <div class="pagination__total-items">
+        of <span class="available_pages">${totalPages}</span>
+      </div>
+      <a href="#documents" onclick="fetchDocs(${previous})">
+        <div class="previous-button square-shape ${previous ? '' : 'very-pale'}">
+        <i class="material-icons">navigate_before</i>
+        </div>
+      </a>
+      <a href="#documents" onclick="fetchDocs(${next})">
+        <div class="next-button square-shape ${next ? '' : 'very-pale'}">
+        <i class="material-icons">navigate_next</i>
+        </div>
+      </a>
+  </div>`;
+  $('#docs_pagination').html(markup);
+}
--- assets/js/cms/utils.js
+++ assets/js/cms/utils.js
@@ -1,1 +1,48 @@
 const baseUrl = "http://0.0.0.0:3000/api/v1";
+
+/**
+ * Client wrapper to make CMS calls.
+ *
+ * @param url
+ * @returns {Promise<Response>}
+ */
+function client(url) {
+  return fetch(`${baseUrl}/${url}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+/**
+ * Format date to natch mock up format.
+ *
+ * @param date
+ * @returns {string}
+ */
+function formatDate(date) {
+  return date.toDateString()
+}
+
+/**
+ *  Extract url parameters from an object
+ *  params {Object} params
+ */
+function formatObjectToParams(params) {
+  let url = '';
+  if (typeof params === 'object') {
+    if (Object.keys(params).length >= 1) {
+      Object.keys(params).forEach((key) => {
+        url += `${key}=${params[key]}&`;
+      });
+      // remove last &
+      url = url.slice(0, -1);
+    }
+  }
+  return url;
+}
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
