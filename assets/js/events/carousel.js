bb70521c1cad66f8fe430183afd500d39df53c03
Black Developa <sylvaelendu91@gmail.com>
2019-06-03 09:00:27 +0100

    bg(event): fix view all events on landing page (#59)
    - fixed carousel slide
    - maintained responsiveness
    - increase max-height for description body
    - remove eol in index.css
    
    [finishes #MMDP-589]

--- assets/js/events/carousel.js
+++ assets/js/events/carousel.js
@@ -15,64 +15,61 @@
       const slide_groups = response.data.map(function (e, i) {
         return i % chunk_size === 0 ? response.data.slice(i, i + chunk_size) : null;
       }).filter(function (e) { return e; });
-
+    
       slide_groups.slice(0, 3).map((item) => {
         slideHtml += `
-                <div class="Slides_show">
-                <div class="allSlides">
-                <div class="prev">
-                  <img src="assets/images/index/group-4-copy-2.png"
-                    srcset="assets/images/index/group-4-copy-3@2x.png 2x, assets/images/group-4-copy-3@3x.png 3x"
-                    alt="arrow for next event" onclick="plusSlides(-1)">
-                </div>
-                <div class="slide fade">
-                  <div class="event_slide_card">
-                    <img src="${item[0].headerImage.url}" />
-                    <div class="event_slide_card_description">
-                      <div class="event-description__rectangle_blue"></div>
-                      <div class="card-content">
-                        <p class="justifying-the-mmdp">${item[0].title}</p>
-                        <h6 class="event_date">${new Date(item[0].eventDate).toDateString()}</h6>
-                        <div class="events-card__desc-body brief-description">
-                          ${item[0].details}
-                        </div>
-                        <br />
-                        <p class="event-description-left__continue-reading">
-                          <a href="event-info.html?id=${item[0]._id}"> Continue Reading </a>
-                        </p>
-                      </div>
+        <div class="Slides_show">
+          <div class="allSlides">
+          <div class="prev">
+          <img src="assets/images/index/group-4-copy-2.png"
+            srcset="assets/images/index/group-4-copy-3@2x.png 2x, assets/images/group-4-copy-3@3x.png 3x"
+            alt="arrow for next event" onclick="plusSlides(-1)">
+          </div>
+            <div class="slide fade">
+              <div class="event_slide_card">
+              <img src="${item[0].headerImage.url}" />
+                <div class="event_slide_card_description">
+                  <div class="event-description__rectangle_blue"></div>
+                  <div class="card-content">
+                    <p class="justifying-the-mmdp">${item[0].title}</p>
+                    <h6 class="event_date">${new Date(item[0].eventDate).toDateString()}</h6>
+                    <div class="events-card__desc-body brief-description">
+                      ${item[0].details}
                     </div>
+                    <br />
+                    <p class="event-description-left__continue-reading">
+                      <a href="event-info.html?id=${item[0]._id}"> Continue Reading </a>
+                    </p>
                   </div>
-                </div>
-      
-                <div class="slide fade">
-                  <div class="event_slide_card">
-                    <img src="${item[1].headerImage.url}" />
-                    <div class="event_slide_card_description">
-                      <div class="event-description__rectangle_blue"></div>
-                      <div class="card-content">
-                        <p class="justifying-the-mmdp">${item[1].title}</p>
-                        <h6 class="event_date"> ${new Date(item[1].eventDate).toDateString()}</h6>
-                        <div class="brief-description events-card__desc-body">
-                        ${item[1].details}
-                        </div>
-                        <br />
-                        <p class="event-description-left__continue-reading">
-                          <a href="event-info.html?id=${item[1]._id}"> Continue Reading </a>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="next">
-                  <img src="assets/images/index/group-4-copy-2.png"
-                    srcset="assets/images/index/group-4-copy-2@2x.png 2x,assets/images/group-4-copy-2@3x.png 3x" class=""
-                    alt="arrow for next event" onclick="plusSlides(1)">
-                </div>
                 </div>
               </div>
-                `;
-
+            </div>
+            <div class="slide fade">
+            <div class="event_slide_card">
+            <img src="${item[1].headerImage.url}" />
+              <div class="event_slide_card_description">
+                <div class="event-description__rectangle_blue"></div>
+                <div class="card-content">
+                  <p class="justifying-the-mmdp">${item[1].title}</p>
+                  <h6 class="event_date">${new Date(item[1].eventDate).toDateString()}</h6>
+                  <div class="events-card__desc-body brief-description">
+                    ${item[1].details}
+                  </div>
+                  <br />
+                  <p class="event-description-left__continue-reading">
+                    <a href="event-info.html?id=${item[1]._id}"> Continue Reading </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="next">
+          <img src="assets/images/index/group-4-copy-2.png"
+            srcset="assets/images/index/group-4-copy-2@2x.png 2x,assets/images/group-4-copy-2@3x.png 3x" class=""
+            alt="arrow for next event" onclick="plusSlides(1)">
+        </div>
+          </div>
+          </div>`;
       });
 
       $('#slideshtml').html(slideHtml);
@@ -82,18 +79,18 @@
       function plusSlides(n) {
         showSlides(slideIndex += n);
       }
-
+      
       function currentSlide(n) {
         showSlides(slideIndex = n);
       }
-
+      
       function showSlides(n) {
         let i;
         let slides = document.getElementsByClassName("Slides_show");
         let dots = document.getElementsByClassName("dot");
-        if (n > slides.length) { slideIndex = 1 }
+        if (n > slides.length) { slideIndex = 1 }   
         if (n < 1) { slideIndex = slides.length }
-        for (i = 0; i < slides.length; i++) {
+        for (i = 0; i < slides.length; i++) {          
           slides[i].style.display = "none";
         }
         slideIndex++;
@@ -101,7 +98,7 @@
         for (i = 0; i < dots.length; i++) {
           dots[i].className = dots[i].className.replace(" active1", "");
         }
-        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 1].style.display = "block";  
         dots[slideIndex - 1].className += " active1";
         setTimeout(showSlides, 3000);
       }
