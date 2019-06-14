9b1e58ed3242060e2ce86d18030670332a70ff34
blackdevelopa <blackdevelopa@gmail.com>
2019-06-14 08:28:29 +0100

    bg(landing page): fixes styling on landing page     - fixes the max screen resolution to a 90%     - fixes events not displaying when the total number is odd
        [Finishes MMDP]

--- assets/js/events/carousel.js
+++ assets/js/events/carousel.js
@@ -13,11 +13,44 @@
       let slideHtml = '';
       const chunk_size = 2;
       const slide_groups = response.data.map(function (e, i) {
-        return i % chunk_size === 0 ? response.data.slice(i, i + chunk_size) : response.data.slice(i);
+        return i % chunk_size === 0 ? response.data.slice(i, i + chunk_size) : null;        
       }).filter(function (e) { return e; });  
                 
-      slide_groups.slice(0, 1).map((item) => {
-        
+      slide_groups.slice(0, 4).map((item) => {
+        if(item.length === 1) {
+          slideHtml += `
+          <div class="Slides_show">
+            <div class="allSlides">
+              <div class="prev">
+              <img src="assets/images/index/group-4-copy-2.png"
+              srcset="assets/images/index/group-4-copy-3@2x.png 2x, assets/images/group-4-copy-3@3x.png 3x"
+              alt="arrow for next event" onclick="plusSlides(-1)">
+              </div>
+                <div class="fade">
+                  <div class="event_slide_card">
+                  <img src="${item[0].headerImage.url}" />
+                    <div class="event_slide_card_description">
+                      <div class="event-description__rectangle_blue"></div>
+                      <div class="card-content">
+                        <p class="justifying-the-mmdp">${item[0].title}</p>
+                        <h6 class="event_date">${new Date(item[0].eventDate).toDateString()}</h6>
+                        <div class="events-card__desc-body brief-description">
+                        ${item[0].details}
+                        </div>
+                        <br />
+                        <p class="event-description-left__continue-reading">
+                          <a href="event-info.html?id=${item[0]._id}"> Continue Reading </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="next">
+                <img src="assets/images/index/group-4-copy-2.png"
+                srcset="assets/images/index/group-4-copy-2@2x.png 2x,assets/images/group-4-copy-2@3x.png 3x" class=""
+                alt="arrow for next event" onclick="plusSlides(1)">
+              </div>`
+        } else {
         slideHtml += `
         <div class="Slides_show">
           <div class="allSlides">
@@ -71,13 +104,15 @@
             </div>
           </div>
         </div>`;
-        });
+        }});
 
       $('#slideshtml').html(slideHtml);
       let slideIndex = 1;
       showSlides(slideIndex);
 
       function plusSlides(n) {
+        console.log('ut');
+        
         showSlides(slideIndex += n);
       }
       
@@ -91,16 +126,16 @@
         let dots = document.getElementsByClassName("dot");
         if (n > slides.length) { slideIndex = 1 }   
         if (n < 1) { slideIndex = slides.length }
-        for (i = 0; i < slides.length; i++) {          
+        for (i = 0; i < slides.length; i++) {                    
           slides[i].style.display = "none";
         }
         slideIndex++;        
         if (slideIndex > slides.length) { slideIndex = 1 }
         for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active1", "");
+          dots[i].className = dots[i].className.replace(" active", "");
         }
         slides[slideIndex - 1].style.display = "block";  
-        dots[slideIndex - slideIndex].className += " active1";
+        dots[slideIndex - 1].className += " active";
         setTimeout(showSlides, 3000);
       }
 
