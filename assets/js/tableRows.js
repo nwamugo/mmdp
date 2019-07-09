4d3edb5da7b7ca076c344407e261748e65bf367d
Rachel Maina <rachel.maina@andela.com>
2019-07-09 10:27:20 +0300

    bg(partnership/collaboration table): fix changing values  - fix looping on array returning endpoint data  [Finishes MMDP-667]

--- assets/js/tableRows.js
+++ assets/js/tableRows.js
@@ -1,63 +1,63 @@
 getTableRows = (data, keys) => {
   tableRows = {
     portentialPartnershipsTableRows: `
-    <main id="table" class="table-row body">
-    <label class="col-1">
-    <input id=${data[keys[0]]} data-org=${
+      <main id="table" class="table-row body">
+      <label class="col-1">
+      <input id=${data[keys[0]]} data-org=${
       data[keys[5]]
     } name="collaboration" value="collaboration" type="checkbox" class="check"/>
-    </label>
-    <div class="col-2"> <div>${data[keys[0]]}</div></div>
-    <div class="col-3"><div id="loading" class="focusTables">${
-      data[keys[2]]
-    }</div></div>
-    <div class="col-4"><div>${data[keys[3]]}</div></div>
-    <div class="col-5"><div>${data[keys[4]]}</div></div>
-    </main>
-        `,
+      </label>
+      <div class="col-2"> <div>${data[keys[0]]}</div></div>
+      <div class="col-3"><div id="loading" class="focusTables">${
+        data[keys[2]]
+      }</div></div>
+      <div class="col-4"><div>${data[keys[3]]}</div></div>
+      <div class="col-5"><div>${data[keys[4]]}</div></div>
+      </main>
+          `,
     gapAnalysisTableRows: `
-        <main id="${data.id}" class="table-row body gap-analysis-details">
-        <label class="report-col-1">
-        <div>
-        <input id=${data[keys[1]].replace(/ /g, '')} data-org=${data[
+          <main id="${data.id}" class="table-row body gap-analysis-details">
+          <label class="report-col-1">
+          <div>
+          <input id=${data[keys[1]].replace(/ /g, '')} data-org=${data[
       keys[1]
     ].replace(/ /g, '')} type="checkbox" name="aaaaa" class="check">
-        </div>
-        </label>
-        <div class="report-col-2"> <div>${data[keys[0]]}</div></div>
-        <div class="report-col-3"><div>${data[keys[1]]}</div></div>
-        <div class="report-col-4"><div>${data[keys[2]]}</div></div>
-        <div class="report-col-5"><div>${data[keys[3]]}</div></div>
-        </main>
-        `,
+          </div>
+          </label>
+          <div class="report-col-2"> <div>${data[keys[0]]}</div></div>
+          <div class="report-col-3"><div>${data[keys[1]]}</div></div>
+          <div class="report-col-4"><div>${data[keys[2]]}</div></div>
+          <div class="report-col-5"><div>${data[keys[3]]}</div></div>
+          </main>
+          `,
     stakeholderDirectoryTableRows: `
-        <tr>
-            <td class="organisation__name">
-                <input id=${data[keys[0]].replace(/ /g, '')} data-org=${data[
+          <tr>
+              <td class="organisation__name">
+                  <input id=${data[keys[0]].replace(/ /g, '')} data-org=${data[
       keys[0]
     ].replace(/ /g, '')} name="aaaaa" value="aaaaa" type="checkbox" />
-                <div class="stakeholder__name">${data[keys[0]]}</div>
-            </td>
-            <td data-target="modal1" class="modal-trigger"  onClick="getSHDetails('${
-              data[keys[0]]
-            }')">${data[keys[1]]}</td>
-            <td data-target="modal1" class="modal-trigger focusTable" onClick="getSHDetails('${
-              data[keys[0]]
-            }')">${data[keys[2]]}</td>
-            <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
-              data[keys[0]]
-            }')">${data[keys[3]]}</td>
-            <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
-              data[keys[0]]
-            }')" id=${data.id}>${data[keys[4]]}</td>
-            <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
-              data[keys[0]]
-            }')">${data[keys[5]]}</td>
-            <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
-              data[keys[0]]
-            }')">${data[keys[6]]}</td>
-        </tr>
-        `
+                  <div class="stakeholder__name">${data[keys[0]]}</div>
+              </td>
+              <td data-target="modal1" class="modal-trigger"  onClick="getSHDetails('${
+                data[keys[0]]
+              }')">${data[keys[1]]}</td>
+              <td data-target="modal1" class="modal-trigger focusTable" onClick="getSHDetails('${
+                data[keys[0]]
+              }')">${data[keys[2]]}</td>
+              <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
+                data[keys[0]]
+              }')">${data[keys[3]]}</td>
+              <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
+                data[keys[0]]
+              }')" id=${data.id}>${data[keys[4]]}</td>
+              <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
+                data[keys[0]]
+              }')">${data[keys[5]]}</td>
+              <td data-target="modal1" class="modal-trigger" onClick="getSHDetails('${
+                data[keys[0]]
+              }')">${data[keys[6]]}</td>
+          </tr>
+          `
   };
   return tableRows;
 };
