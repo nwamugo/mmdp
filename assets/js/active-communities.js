34dfe4f0eb0bad7a36ccfee344325e7fedb2ac1d
BukkyOmo <odunayobukky1@gmail.com>
2019-08-23 03:27:41 +0100

    view active communities without page reload

--- assets/js/active-communities.js
+++ assets/js/active-communities.js
@@ -1,38 +1,30 @@
 (function() {
   let MMDP_BASE_URL;
   if (
-    window.location.host.includes('127.0.0.1') ||
-    window.location.host.includes('localhost')
+    window.location.host.includes("127.0.0.1") ||
+    window.location.host.includes("localhost")
   ) {
-    MMDP_BASE_URL = 'http://localhost:3000';
+    MMDP_BASE_URL = "http://localhost:3000";
   } else {
-    MMDP_BASE_URL = 'http://cms-staging.mmdp.ng:3000';
+    MMDP_BASE_URL = "http://cms-staging.mmdp.ng:3000";
   }
 
   let lgaName;
   let stateName;
   const baseURL = window.location.host;
-  const SVG_NS = 'http://www.w3.org/2000/svg';
+  const SVG_NS = "http://www.w3.org/2000/svg";
 
-  const link = document.getElementById('hide-active');
-  link.addEventListener(
-    'click',
-    () =>
-      (window.location.href = `http://${baseURL}/lga.html?lga=${lgaName}&state=${stateName}`)
-  );
+  const backToState = document.getElementById("back-state");
 
-  const backToState = document.getElementById('back-state');
+  stateName = getqueryName("stateName");
 
-  stateName = getqueryName('stateName');
-
-  backToState.innerText = `Back to ${stateName.replace('%20', ' ')} state`;
-
+  backToState.innerText = `Back to ${stateName.replace("%20", " ")} state`;
   backToState.addEventListener(
-    'click',
+    "click",
     () =>
       (window.location.href = `http://${baseURL}/state.html?state=${stateName.replace(
-        '%20',
-        ' '
+        "%20",
+        " "
       )}`)
   );
 
@@ -45,11 +37,13 @@
         `${MMDP_BASE_URL}/api/v1/location?${query}&focusAreaName`
       );
       const data = await stakeholderData.json();
+
       const {
         beneficiaryServicesCount,
         stakeholderCount,
-        focusAreasCount,
+        focusAreasCount
       } = data;
+
       const communities = data.filteredStakeholders.map(community => {
         return community.beneficiaries[0].communities;
       });
@@ -57,6 +51,7 @@
       const commArray = communities.reduce(function(arr, e) {
         return arr.concat(e);
       });
+
       const commName = commArray.map(name => {
         return name.communityId.communityName;
       });
@@ -70,6 +65,7 @@
         focusAreasCount,
         uniqueNames
       );
+
       return counts;
     } catch (error) {}
   }
@@ -86,7 +82,7 @@
 
   // a function to draw a circle
   function drawCircle(o, parent) {
-    var circle = document.createElementNS(SVG_NS, 'circle');
+    var circle = document.createElementNS(SVG_NS, "circle");
     for (var name in o) {
       if (o.hasOwnProperty(name)) {
         circle.setAttributeNS(null, name, o[name]);
@@ -96,44 +92,81 @@
     return circle;
   }
 
+  function setAttributes(node, attributes) {
+    for (var name in attributes) {
+      if (attributes.hasOwnProperty(name)) {
+        node.setAttributeNS(null, name, attributes[name]);
+      }
+    }
+  }
+
+  async function drawSvg(o, parent, number) {
+    var circle = document.createElementNS(SVG_NS, "circle");
+    var group = document.createElementNS(SVG_NS, "g");
+    var text = document.createElementNS(SVG_NS, "text");
+
+    const textAttributes = {
+      "text-anchor": "middle",
+      stroke: "black",
+      "stroke-width": 0.001,
+      fill: "#1A1F37",
+      "font-family": "Muli-ExtraBold, Muli",
+      "font-size": 0.03,
+      "font-weight": 200,
+      "letter-spacing": 0.001,
+      x: o.cx,
+      y: o.cy + 0.012
+    };
+
+    setAttributes(circle, o);
+    setAttributes(text, textAttributes);
+
+    text.textContent = number;
+
+    group.appendChild(circle);
+    group.appendChild(text);
+    parent.appendChild(group);
+    return circle;
+  }
+
   function appendMap(path) {
-    d3.select('#svg-container')
-      .append('svg')
-      .attr('fill', 'none')
-      .attr('height', 584)
-      .attr('width', 521)
-      .attr('id', 'svg')
-      .attr('transform', 'scale(1.0)')
-      .append('path')
-      .attr('id', 'thePath')
-      .attr('class', 'path')
-      .attr('d', path)
-      .attr('fill', '#bad9e3')
-      .attr('stroke', 'none')
-      .attr('stroke-width', 1);
+    d3.select("#svg-container")
+      .append("svg")
+      .attr("fill", "none")
+      .attr("height", 584)
+      .attr("width", 521)
+      .attr("id", "svg")
+      .attr("transform", "scale(1.0)")
+      .append("path")
+      .attr("id", "thePath")
+      .attr("class", "path")
+      .attr("d", path)
+      .attr("fill", "#bad9e3")
+      .attr("stroke", "none")
+      .attr("stroke-width", 1);
   }
 
   function appendDefs() {
     // appending the circle to show focus areas count
-    d3.select('svg')
-      .append('defs')
-      .append('symbol')
-      .attr('id', 'mark')
-      .attr('viewBox', '1.2 1.7 16 16')
-      .append('g')
-      .attr('transform', 'translate(0.0625 0.0475)')
-      .append('circle')
-      .attr('r', 0.5)
-      .attr('cx', 8)
-      .attr('cy', 8)
-      .attr('fill', 'red')
-      .attr('stroke', 'red')
-      .attr('stroke-width', 0.001);
+    d3.select("svg")
+      .append("defs")
+      .append("symbol")
+      .attr("id", "mark")
+      .attr("viewBox", "1.2 1.7 16 16")
+      .append("g")
+      .attr("transform", "translate(0.0625 0.0475)")
+      .append("circle")
+      .attr("r", 0.5)
+      .attr("cx", 8)
+      .attr("cy", 8)
+      .attr("fill", "red")
+      .attr("stroke", "red")
+      .attr("stroke-width", 0.001);
   }
 
   function getCoordinates(count) {
     let points = [];
-    const lgaPath = document.getElementById('thePath');
+    const lgaPath = document.getElementById("thePath");
     // svg client rect
     let cr = lgaPath.getBoundingClientRect();
 
@@ -152,14 +185,14 @@
       //elementFromPoint returns the topmost Element at the specified coordinates (relative to the viewport).
       let elmt = document.elementFromPoint(x, y);
       // if the point is in path
-      if (elmt && elmt.className.baseVal === 'path' && elmt.id === 'thePath') {
+      if (elmt && elmt.className.baseVal === "path" && elmt.id === "thePath") {
         //get the coordinates of the point on the svg
         let svgPoint = getPoint(x, y);
         //draw a circle with the center on the svg point
 
         points.push({
           cx: svgPoint.x,
-          cy: svgPoint.y,
+          cy: svgPoint.y
         }),
           //increase the counter
           n++;
@@ -172,10 +205,24 @@
     return points;
   }
 
+  function getCoord() {
+    const lgaPath = document.getElementById("thePath");
+
+    // svg client rect
+    let cr = lgaPath.getBBox();
+
+    let x = cr.x + cr.width * 0.5;
+    let y = cr.y + cr.height * 0.5;
+
+    let x1 = cr.x + cr.width * 0.5 + 0.052;
+
+    return [{ x, y }, { x: x1, y }];
+  }
+
   async function loaded() {
-    lgaName = getqueryName('lgaName');
-    const lgaSpan = document.getElementById('lga-name');
-    lgaSpan.innerHTML = lgaName.replace('%20', ' ');
+    lgaName = getqueryName("lgaName");
+    const lgaSpan = document.getElementById("lga-name");
+    lgaSpan.innerHTML = lgaName.replace("%20", " ");
     try {
       const responsePromise = await fetch(
         `${MMDP_BASE_URL}/api/v1/matrix/lga?name=${lgaName}`
@@ -187,45 +234,122 @@
       }
 
       appendMap(path);
-      const svg = document.getElementById('svg');
+      const svg = document.getElementById("svg");
 
       let bb = thePath.getBBox();
       //set the svg viewBox attribute
       svg.setAttributeNS(
         null,
-        'viewBox',
+        "viewBox",
         `${bb.x} ${bb.y} ${bb.width} ${bb.height}`
       );
       appendDefs();
-      let stakeName;
+
+      $("#show-active-div").click(function() {
+        $("#show-active-div").hide();
+        $("#lga-report-button").hide();
+        $(".marker").hide();
+        $("#hide-active-div").toggle();
+
+        fetchCommunities(MMDP_BASE_URL, lgaName, true);
+      });
+
+      $("#hide-active-div").click(function() {
+        $("#hide-active-div").hide();
+        $("g").hide();
+        $("#show-active-div").toggle();
+        $("#lga-report-button").show();
+        $(".marker").show();
+
+        fetchCommunities(MMDP_BASE_URL, lgaName);
+      });
 
       // add markers
-      const servicesCount = await fetchCount();
-      var myStringArray = servicesCount[3];
-      var arrayLength = myStringArray.length;
-      for (var i = 0; i < arrayLength; i++) {
-        //Do something
-        communityName = myStringArray[i];
+      let servicesCount = await fetchCount();
+      if ((servicesCount)) {
+        var myStringArray = servicesCount[3];
+        var arrayLength = myStringArray.length;
+        for (var i = 0; i < arrayLength; i++) {
+          communityName = myStringArray[i];
+        }
+      } else {
+        return false;
       }
 
       const points = getCoordinates(arrayLength);
-      for (let i = 0; i < points.length; i++) {
-        drawCircle(
-          {
-            communityName: myStringArray[i],
-            fill: 'red',
-            class: 'marker',
-            cx: points[i].cx,
-            cy: points[i].cy,
-            r: 0.004,
-          },
-          svg
-        );
+      if (points.length > 0) {
+        for (let i = 0; i < points.length; i++) {
+          drawCircle(
+            {
+              communityName: myStringArray[i],
+              fill: "red",
+              class: "marker",
+              cx: points[i].cx,
+              cy: points[i].cy,
+              r: 0.004
+            },
+            svg
+          );
+        }
       }
 
-      $('.marker')
+      async function fetchCommunities(MMDP_BASE_URL, lgaName, showBtn = false) {
+        lgaName = getqueryName("lgaName");
+        const lgaSpan = document.getElementById("lga-name");
+        const spaceEncoding = /%20/gi;
+        lgaSpan.innerHTML = lgaName.replace(spaceEncoding, " ");
+        try {
+          const responsePromise = await fetch(
+            `${MMDP_BASE_URL}/api/v1/matrix/lga?name=${lgaName}`
+          );
+          const response = await responsePromise.json();
+          const { name, path } = response.data[0];
+          if (!name) {
+            window.location.href = `http://${baseURL}/state.html`;
+          }
+
+          appendMap(path);
+          const svg = document.getElementById("svg");
+
+          const thePath = document.getElementById("thePath");
+
+          let bb = thePath.getBBox();
+          //set the svg viewBox attribute
+          svg.setAttributeNS(
+            null,
+            "viewBox",
+            `${bb.x} ${bb.y} ${bb.width} ${bb.height}`
+          );
+
+          // add markers
+          const points = getCoord();
+
+          const counts = await fetchCount();
+          let fill = ["red", "green"];
+          if (points.length > 0 && showBtn) {
+            for (let i = 0; i <= points.length; i++) {
+              drawSvg(
+                {
+                  fill: "#FFF",
+                  class: "marker",
+                  id: "remove",
+                  stroke: fill[i],
+                  "stroke-width": 0.005,
+                  cx: points[i].x - 0.034,
+                  cy: points[i].y,
+                  r: 0.02
+                },
+                svg,
+                counts[i]
+              );
+            }
+          }
+        } catch (error) {}
+      }
+
+      $(".marker")
         .mouseover(function(e) {
-          var community = $(this).attr('communityName') || '';
+          var community = $(this).attr("communityName") || "";
           var stakeholders = servicesCount[1];
           var focusarea = servicesCount[2];
           var services = servicesCount[0];
@@ -233,34 +357,34 @@
             '<div class="info_panel">' +
               '<div class="community">Community Name: ' +
               community +
-              '</div><br>' +
+              "</div><br>" +
               '<div class="counts">Stakeholders: ' +
               stakeholders +
-              '<br>' +
-              'Focus Area: ' +
+              "<br>" +
+              "Focus Area: " +
               focusarea +
-              '<br>' +
-              'Services: ' +
+              "<br>" +
+              "Services: " +
               services +
-              '</div><br>' +
-              '</div>'
-          ).appendTo('body');
+              "</div><br>" +
+              "</div>"
+          ).appendTo("body");
         })
         .mouseleave(function() {
-          $('.info_panel').remove();
+          $(".info_panel").remove();
         })
         .mousemove(function(e) {
           var mouseX = e.pageX,
             mouseY = e.pageY;
 
-          $('.info_panel').css({
+          $(".info_panel").css({
             top: mouseY - 50,
-            left: mouseX - $('.info_panel').width() / 2,
+            left: mouseX - $(".info_panel").width() / 2
           });
         });
     } catch (error) {
       throw error;
     }
   }
-  document.addEventListener('DOMContentLoaded', loaded, true);
+  document.addEventListener("DOMContentLoaded", loaded, true);
 })();
